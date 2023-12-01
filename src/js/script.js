cabd8f5a5a3629efb2f5321d950f2c904e6560e4
João Henrique <santos.joao@crmsolucoes.com.br>
2023-12-01 09:15:22 -0300

    3 - A função ready

--- src/js/script.js
+++ src/js/script.js
@@ -1,25 +1,6 @@
-//  JavaScript puro
+//  Obs. Esse script está sendo importado na tag <head></head>.
 
-let btnJS = document.querySelector('#btn_somarjs').addEventListener('click', () => {
-
-    let v1 = document.querySelector('#v1').value;
-    let v2 = document.querySelector('#v2').value;
-
-    document.querySelector('#res').value = (parseInt(v1) + parseInt(v2))
-
-});
-
-
-//  jQuery
-
-//  Seleciona usando a sintaxe $()
-//  .val() retorna o valor do elemento selecionado
-//  parseInt() converte o valor do elemento selecionado em um inteiro
-
-$("#btn_somarjq").click(() => {
-
-    let v1 = $("#v1").val();
-    let v2 = $("#v2").val();
-
-    $("#res").val(parseInt(v1) + parseInt(v2));
+//  Seleciona o documento e adiciona uma arrow function que roda quando a página for carregada
+$(document).ready(() => {
+    $("#texto").text("Esse texto foi adicionado com jQuery")
 })
