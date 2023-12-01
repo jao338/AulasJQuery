d07e1a56f87b98f347c92b8f32f0060013b62c5b
João Henrique <santos.joao@crmsolucoes.com.br>
2023-12-01 08:59:49 -0300

    2 - Selecionando e mudando valores com JS puro e jQuery

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,25 @@
-$("p").css("color", "#f00")
+//  JavaScript puro
+
+let btnJS = document.querySelector('#btn_somarjs').addEventListener('click', () => {
+
+    let v1 = document.querySelector('#v1').value;
+    let v2 = document.querySelector('#v2').value;
+
+    document.querySelector('#res').value = (parseInt(v1) + parseInt(v2))
+
+});
+
+
+//  jQuery
+
+//  Seleciona usando a sintaxe $()
+//  .val() retorna o valor do elemento selecionado
+//  parseInt() converte o valor do elemento selecionado em um inteiro
+
+$("#btn_somarjq").click(() => {
+
+    let v1 = $("#v1").val();
+    let v2 = $("#v2").val();
+
+    $("#res").val(parseInt(v1) + parseInt(v2));
+})
