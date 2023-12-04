9f5889e3988c0b093ae1d2b28c6f5b408f4ff63e
João Henrique <santos.joao@crmsolucoes.com.br>
2023-12-04 08:49:38 -0300

    12 - Métodos children e find

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,7 @@
-//  Adiciona uma borda ao elemento pai do elemento "p", neste caso é o elemento "li"
-$("p").parent().css({"border" : "1px solid #f00"})
+//  Adiciona uma borda ao(s) elemento(s) filho(s) direto(s)
+$("div").children().css({"border" : "1px solid #f00"})
 
-//  Neste caso um borda é adicionada ao elemento "ul"
-$("li").parent().css({"border" : "1px solid #00f"})
-
-//  Neste é adicionado ao elemento "div"
-$("ul").parent().css({"border" : "1px solid #0f0"})
-
-//  Ao usar "parents()" todos os elementos pais são selecionados
-$("p").parents().css({"border" : "1px solid #f00"})
-
-//  Ao usar "parentsUntil()" são selecionados todos os elementos pais de um elemento até encontrar um determinado elemento
-$("p").parentsUntil("div").css({"border" : "1px solid #00f"})
+//  Adiciona uma borda ao(s) elemento(s) filho(s) especificados.
+//  É possível selecionar o primeiro, o último e todos os elementos filhos do elemento selecionado usando "first", "last" ou "*"
+$("div").find("ul:first").css({"border" : "1px solid #0f0"})
 
