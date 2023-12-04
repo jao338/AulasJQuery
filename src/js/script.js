929ecd4af8754c4b2bcca4d1b67d5a8307dafd8d
João Henrique <santos.joao@crmsolucoes.com.br>
2023-12-04 11:46:25 -0300

    21 - Usando o método animate com o parametro toggle

--- src/js/script.js
+++ src/js/script.js
@@ -1,27 +1,10 @@
-//  Na função "animate()", o primeiro conjunto de parametros definem as propriedades e os valores do(s) elemento(s) selecionado(S). O segundo conjunto de parametros definem as propriedades da animação
-$("#btn-up").click(function(){
-    
-    $("#caixa").animate({width: "500px", height: "500px"}, {duration: 3000})
+$("#btn-mostrar").click(function() {
 
+    $("#caixa").animate({opacity: "toggle"})
 });
 
-$("#btn-down").click(function(){
-    
-    $("#caixa").animate({width: "200px"}).animate({height: "200px"})
+$("#btn-tamanho").click(function() {
 
-});
-
-//  A propriedade "complete" define uma função que é executada após o fim da animação
-$("#btn-right").click(function(){
-    
-    $("#caixa").animate({left: "500px"}, {duration: 1000, complete: () => {
-        alert('Movimentação finalizada')
-    }})
-
-});
-
-$("#btn-left").click(function(){
-    
-    $("#caixa").animate({left: "0px"}, {duration: 1000})
+    $("#caixa").animate({width: "toggle", height: "toggle"})
 
 });
