a9e2a75c88e9454d0c66c4c9faf1b4f1e5d00d97
João Henrique <santos.joao@crmsolucoes.com.br>
2023-12-04 11:27:53 -0300

    20 - O método animate

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,27 @@
-//   O método "show()" exibe o elemento selecionado
-$("#btn-toggle").click(function(){
-    $("#caixa").toggle()
+//  Na função "animate()", o primeiro conjunto de parametros definem as propriedades e os valores do(s) elemento(s) selecionado(S). O segundo conjunto de parametros definem as propriedades da animação
+$("#btn-up").click(function(){
+    
+    $("#caixa").animate({width: "500px", height: "500px"}, {duration: 3000})
 
-    //  Se o elemento "#caixa" tiver a propriedade "visible"
-    if($("#caixa").is(':visible')){
-        $("#btn-toggle").text('Ocultar')
-    }else{
-        $("#btn-toggle").text('Mostrar')
-    }
 });
 
+$("#btn-down").click(function(){
+    
+    $("#caixa").animate({width: "200px"}).animate({height: "200px"})
+
+});
+
+//  A propriedade "complete" define uma função que é executada após o fim da animação
+$("#btn-right").click(function(){
+    
+    $("#caixa").animate({left: "500px"}, {duration: 1000, complete: () => {
+        alert('Movimentação finalizada')
+    }})
+
+});
+
+$("#btn-left").click(function(){
+    
+    $("#caixa").animate({left: "0px"}, {duration: 1000})
+
+});
