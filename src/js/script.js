e4df03b0ae96aa9ff3544bb98ba955b06c47a985
João Henrique <santos.joao@crmsolucoes.com.br>
2023-12-04 09:32:39 -0300

    14 - First, last e eq

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,8 @@
-//  Adiciona uma borda a todos os elementos "irmãos"(que estão no mesmo nível que ele) do elemento selecionado
-$("h3").siblings().css({"border" : "1px solid #f00"})
+//  Usa-se o "first()" para selecionar o primeiro elemento "p" e o "css()" para adicionar formatação
+$("p").first().css({"border" : "1px solid #f00", "background-color" : "#ffa"})
 
-//  É possível especificar quais elementos "irmãos" que devem ser selecionados
-$("h3").siblings("p").css({"border" : "1px solid #0f0"})
+//  Usa-se o "last()" para selecionar o último elemento "p" e o "css()" para adicionar formatação
+$("p").last().css({"border" : "1px solid #f00", "background-color" : "#afa"})
 
-//  Adiciona uma borda ao próximo elemento "irmão" do elemento selecionado. 
-//  É possível utilizar o método "prev()" para selecionar o elemento irmão anterior do elemento especificado
-
-$("h3").next().css({"border" : "1px solid #00f"})
-
-//  Adiciona uma borda a todos os elementos irmãos do elemento selecionado
-//  É possível utilizar o método "prevAll()" para selecionar todos os elementos irmãos anteriores
-$("h3").nextAll().css({"border" : "1px solid #f0f"})
-
-//  Adiciona uma borda a todos os elementos irmãos do elemento especificado até encontrar um certo elemento
-$("h3").nextUntil("h2").css({"border" : "1px solid #f00"})
-
-
+//  Usa-se o "eq()" para selecionar o elemento de index especificado em "eq(x)" e o "css()" para adicionar formatação
+$("p").eq(1).css({"border" : "1px solid #f00", "background-color" : "#aaf"})
