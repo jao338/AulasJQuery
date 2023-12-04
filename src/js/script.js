f05f6e9c66d46f6ce3257da7a09ed1e999815af8
João Henrique <santos.joao@crmsolucoes.com.br>
2023-12-04 11:56:36 -0300

    22 - Usando a função delay

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,17 @@
+//  O método "delay()" define um intervalo de tempo(ms)
+$("#btn-aumentar").click(function() {
+
+    $("#caixa").delay(2000).animate({width: "500px"}).delay(2000).animate({height: "500px"})
+});
+
+$("#btn-diminuir").click(function() {
+
+    $("#caixa").delay(2000).animate({width: "200px"}).delay(2000).animate({height: "200px"})
+});
+
 $("#btn-mostrar").click(function() {
 
-    $("#caixa").animate({opacity: "toggle"})
+    $("#caixa").delay(2000).animate({opacity: "toggle"}).delay(2000).animate({opacity: "toggle"})
 });
 
-$("#btn-tamanho").click(function() {
 
-    $("#caixa").animate({width: "toggle", height: "toggle"})
-
-});
