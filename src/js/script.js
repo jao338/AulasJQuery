3e081b0e0955e3e807049ebc7bfe5649aae8769a
João Henrique <santos.joao@crmsolucoes.com.br>
2023-12-04 08:33:23 -0300

    11 - Métodos parent

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,15 @@
-//  Evita que o(s) evento(s) se propagem. A propagação acontece quando um evento de um elemento filho é acionado pelo elemeno pai
-$("div").click( (event) => {
-    event.stopPropagation();
-    alert('Evento disparado na DIV')
-});
+//  Adiciona uma borda ao elemento pai do elemento "p", neste caso é o elemento "li"
+$("p").parent().css({"border" : "1px solid #f00"})
 
-$("p").click( (event) => {
-    event.stopPropagation();
-    alert('Evento disparado no P')
-});
+//  Neste caso um borda é adicionada ao elemento "ul"
+$("li").parent().css({"border" : "1px solid #00f"})
 
-$("span").click( (event) => {
-    event.stopPropagation();
-    alert('Evento disparado no SPAN')
-});
+//  Neste é adicionado ao elemento "div"
+$("ul").parent().css({"border" : "1px solid #0f0"})
+
+//  Ao usar "parents()" todos os elementos pais são selecionados
+$("p").parents().css({"border" : "1px solid #f00"})
+
+//  Ao usar "parentsUntil()" são selecionados todos os elementos pais de um elemento até encontrar um determinado elemento
+$("p").parentsUntil("div").css({"border" : "1px solid #00f"})
+
