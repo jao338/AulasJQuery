a38a08b005cf835b2ed23411ec74313cd5f4740b
João Henrique <santos.joao@crmsolucoes.com.br>
2023-12-04 10:54:00 -0300

    18 - Os métodos show e hide

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,9 @@
-//  "this" retorna o próprio elemento selecionado
-//  O método "attr()" retorna o atributo especificado do elemento selecionado.
-$("#caixa").click(function(){
-    alert("Selecionando o id: " + $(this).attr('class'))
+//   O método "show()" exibe o elemento selecionado
+$("#btn-mostrar").click(function(){
+    $("#caixa").show()
 });
 
-//   Ao passar mais um parâmetro para a função "attr()" alteramos o valor do atributo especificado
-$("#btn-red").click(function(){
-    $("#caixa").attr('class', 'vermelho')
-});
-
-$("#btn-blue").click(function(){
-    $("#caixa").attr('class', 'azul')
+//   O método "hide()" esconde o elemento selecionado
+$("#btn-ocultar").click(function(){
+    $("#caixa").hide()
 });
