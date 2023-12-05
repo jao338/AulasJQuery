a1a0e4e94de5cad65d37e3b645f7ca23bc3fd1db
João Henrique <santos.joao@crmsolucoes.com.br>
2023-12-05 08:42:31 -0300

    24 - Usando o método fadeTo

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,9 @@
-$("#btn-mostrar").click(function() {
-    $("#caixa").fadeIn(3000);
-});
+//  Adiciona um evento de clique a todos os elementos que possuem a classe "itemMenu". Adicionano, também, um evento que é acionado quando o mouse está sobre o elemento.
+//  O método "fadeTo()" aplica uma animação de opacidade sobre o elemento. Recebendo dois argumentos, sendo o primeiro a duração(ms) e o segundo a opacidade(de 0.0 a 1.0)
+$(".itemMenu").mouseover(function() {
+    $(this).fadeTo(1000, 0.3)
+})
 
-$("#btn-ocultar").click(function() {
-    $("#caixa").fadeOut(3000);
-});
-
-$("#btn-mostrarocultar").click(function() {
-    $("#caixa").fadeOut(5000).fadeIn(1000);
-});
-
+$(".itemMenu").mouseleave(function() {
+    $(this).fadeTo(1000, 1)
+})
