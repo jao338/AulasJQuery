50800809c4a8a3aba40c0c0b498d19c7e869ecc3
João Henrique <santos.joao@crmsolucoes.com.br>
2023-12-05 09:45:41 -0300

    27 - A função queue

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,23 @@
-//  A função "finish()" pula a animação para o final
+//  O método "queue()" é um método que retorna a fila de animações. Com a propriedadde "length" obtemos a quantidade de animações na fla
+//  Toda vez que uma animação terminar a função "text()" é chamada alterando o texto do elemento de id "info"
+const cx = $("#caixa");
+
 $("#btnAumentar").click(function(){
 
-    $("#caixa").animate({width: "100%"}, 5000)
+    $("#caixa").animate({width: "100%"}, {duration: 5000, complete: () => {
+        $("#info").text(cx.queue().length)
+    }})
 
+    $("#info").text(cx.queue().length)
 });
 
 $("#btnDiminuir").click(function(){
 
-    $("#caixa").animate({width: "100px"}, 5000)
+    $("#caixa").animate({width: "100px"}, {duration: 5000, complete: () => {
+        $("#info").text(cx.queue().length)
+    }})
 
+    $("#info").text(cx.queue().length)
 });
 
 $("#btnFinalizar").click(function(){
