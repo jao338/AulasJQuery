320e0dc285f1425ce6cbc584bd56f1fe1d7a4156
João Henrique <santos.joao@crmsolucoes.com.br>
2023-12-06 11:38:05 -0300

    43 - Removendo atributos e classes

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,8 @@
 //  A função "position()" possui dois atributos que recuperam a posição do elemento
 //  A função "change()" aciona uma ação quando o valor do elemento é alterado
 //  A função "offset()" possui dois atributos que recuperam a posição do elemento. Com essa função também é possível alterar os valores das propriedades "top" e "left", diferentemente da função "position()"
+//  A função "removeAttr()" remove o atributo especificado
+//  A função "removeClass()" remove um ou mais classe(s) de um elemento
 $("#btnPosicao").click(() => {
 
     let pt = $("#caixa").position().top
@@ -25,5 +27,17 @@ $("#left").change((event) => {
     let valor = document.getElementById(event.target.id).value
 
     $("#caixa").offset({"left":valor})
+
+});
+
+$("#btnRemoverAtributo").click(() => {
+
+    $("#caixa").removeAttr("style")
+
+});
+
+$("#btnRemoverClasse").click(() => {
+
+    $("#caixa").removeClass("borda")
 
 });
