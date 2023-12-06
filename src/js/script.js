53d2e5647d8e5840c36ba4aacabda7ca96819c72
João Henrique <santos.joao@crmsolucoes.com.br>
2023-12-06 11:23:09 -0300

    42 - Alterando a posição de um elemento usando offset

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,22 @@
 //  A função "position()" possui dois atributos que recuperam a posição do elemento
 //  A função "change()" aciona uma ação quando o valor do elemento é alterado
+//  A função "offset()" possui dois atributos que recuperam a posição do elemento. Com essa função também é possível alterar os valores das propriedades "top" e "left", diferentemente da função "position()"
 $("#btnPosicao").click(() => {
 
-    let t = $("#caixa").position().top
-    let l = $("#caixa").position().left
+    let pt = $("#caixa").position().top
+    let pl = $("#caixa").position().left
+    let ot = $("#caixa").offset().top
+    let ol = $("#caixa").offset().left
 
-    alert("Top: " + t + " | Left: " + l)
+    alert("Position Top: " + pt + " | Position Left: " + pl)
+    alert("Offeset Top: " + ot + " | Offeset Left: " + ol)
 });
 
 $("#top").change((event) => {
 
     let valor = document.getElementById(event.target.id).value
 
-    $("#caixa").css({"top":valor+"px"})
+    $("#caixa").offset({"top":valor})
 
 });
 
@@ -20,6 +24,6 @@ $("#left").change((event) => {
 
     let valor = document.getElementById(event.target.id).value
 
-    $("#caixa").css({"left":valor+"px"})
+    $("#caixa").offset({"left":valor})
 
 });
