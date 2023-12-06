4c34c8aefb715a4f572f111478769cb323725331
João Henrique <santos.joao@crmsolucoes.com.br>
2023-12-06 10:14:03 -0300

    39 - A função detach

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,25 @@
-//  A função "clone()" cria um clone exato do elemento selecionado
-$("#btnClonar").click(() => {
+//  A função "detach()" remove um elemento e o armazena na memória
+let itens = $(".item")
 
-    $("#caixa").clone().appendTo("body");
+let i = 0
 
-}); 
+$("#btnMudar").click(() => {
+
+    let item = $(itens[i]).detach();
+
+    i++;
+
+    $("#caixa2").append(item);
+    
+});
+
+$("#btnReset").click(() => {
+    
+    for (let index = 0; index < itens.length; index++) {
+
+        let item = $(itens[index]).detach();
+        
+        $("#caixa1").append(item);
+    }
+    
+});
