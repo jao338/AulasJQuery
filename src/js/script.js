5bdf1e31032e475cd8c6d62269659b959afa3c46
João Henrique <santos.joao@crmsolucoes.com.br>
2023-12-06 12:06:36 -0300

    44 - Usando as funções replaceAll e replaceWith

--- src/js/script.js
+++ src/js/script.js
@@ -1,43 +1,25 @@
-//  A função "position()" possui dois atributos que recuperam a posição do elemento
-//  A função "change()" aciona uma ação quando o valor do elemento é alterado
-//  A função "offset()" possui dois atributos que recuperam a posição do elemento. Com essa função também é possível alterar os valores das propriedades "top" e "left", diferentemente da função "position()"
-//  A função "removeAttr()" remove o atributo especificado
-//  A função "removeClass()" remove um ou mais classe(s) de um elemento
-$("#btnPosicao").click(() => {
+$("#btnReplace1").click((event) => {
 
-    let pt = $("#caixa").position().top
-    let pl = $("#caixa").position().left
-    let ot = $("#caixa").offset().top
-    let ol = $("#caixa").offset().left
-
-    alert("Position Top: " + pt + " | Position Left: " + pl)
-    alert("Offeset Top: " + ot + " | Offeset Left: " + ol)
-});
-
-$("#top").change((event) => {
-
-    let valor = document.getElementById(event.target.id).value
-
-    $("#caixa").offset({"top":valor})
+    $("<p>Esse texto foi adicionado com replaceAll()</p>").replaceAll(".cor")
 
 });
 
-$("#left").change((event) => {
+$("#btnReplace2").click((event) => {
 
-    let valor = document.getElementById(event.target.id).value
-
-    $("#caixa").offset({"left":valor})
+    $("<div>"+$(event.currentTarget).text()+"</div>").replaceAll(event.currentTarget)
 
 });
 
-$("#btnRemoverAtributo").click(() => {
+$("#btnReplace3").click((event) => {
 
-    $("#caixa").removeAttr("style")
+    
+    $(".cor").replaceWith("<p>Texto trocado</p>")
 
 });
 
-$("#btnRemoverClasse").click(() => {
+$("#btnReplace3").click((event) => {
 
-    $("#caixa").removeClass("borda")
+    
+    $(event.currentTarget).replaceWith("<div>"+$(event.currentTarget).text()+"</div>")
 
 });
