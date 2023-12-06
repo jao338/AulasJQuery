9ec421095805527deef03a2640807e8bb6a3362c
João Henrique <santos.joao@crmsolucoes.com.br>
2023-12-06 09:22:02 -0300

    38 - A função clone

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
-//  Use a função "setInterval()" para definir uma funçaõ que será executada a cada intervalo de tempo especificado
-//  A função "toggleClass()" adiciona a classe especificada caso o elemento não possua a classa ou a remove caso já a possua
-let alert = setInterval(() => {
-    $("#caixa").toggleClass("alerta")
-}, 1000)
+//  A função "clone()" cria um clone exato do elemento selecionado
+$("#btnClonar").click(() => {
 
+    $("#caixa").clone().appendTo("body");
+
+}); 
